d9fac30628e1a174856688e8212704aa491e9ede
Richard Jerry <10014429@sbschools.org>
2018-11-29 12:40:14 -0500

    first method done
    richard

--- MethodPracticeGit.java
+++ MethodPracticeGit.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
 public class MethodPracticeGit{
 	public static void main (String[]args){
-	
-	
+
+		System.out.println(ConvertToArrayList(8950));
 	}
+
+	public static ArrayList <Integer> ConvertToArrayList(int value){
+
+		ArrayList <Integer> list = new ArrayList <> ();
+
+		while(value/10!=0){
+
+			list.add(value%10);
+			value/=10;
+
+
+			}
+
+			list.add(value);
+
+		for (int i = list.size()- 2; i > -1; i--)
+			list.add(list.remove(i));
+
+		return list;
+
+	}
+
+
+
+
+
+
 	}
